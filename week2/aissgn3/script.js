b10da44748f0301beed987536bdb71732c773827
Yeonseo-Jo <jys073190@gmail.com>
2023-10-25 05:01:15 +0900

    feat : top 버튼 스크롤에 따라 투명도 조절 구현

--- week2/aissgn3/script.js
+++ week2/aissgn3/script.js
@@ -27,4 +27,16 @@ const handleShowDescription = () => {
   firstSectionImgContainer.addEventListener("mouseout", delDescription);
 };
 
+// ***** top 버튼 스크롤 내림에 따라 선명해지게 구현 ******
+const handleTopBtnOpacity = () => {
+  const topBtn = $(".to-top");
+  //top 버튼과 뷰포트의 상대적인 위치 정보를 받아옴
+  const topBtnHeight = topBtn.getBoundingClientRect().height;
+
+  document.addEventListener("scroll", () => {
+    topBtn.style.opacity = window.scrollY / topBtnHeight / 30;
+  });
+};
+
 handleShowDescription();
+handleTopBtnOpacity();
