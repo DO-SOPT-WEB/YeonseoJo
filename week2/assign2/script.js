8e8c58e3c410168d36655c67c3b5702975493884
Yeonseo-Jo <jys073190@gmail.com>
2023-10-26 02:29:23 +0900

    feat : 수입/지출 필터링 구현

--- week2/assign2/script.js
+++ week2/assign2/script.js
@@ -50,4 +50,29 @@ const handleRenderInitData = () => {
   renderTotalBalance(); // 총 수입, 지출, 자산을 데이터로부터 가져와 보여준다
 };
 
+//체크 박스 선택에 따른 필터링 구현
+const incomeCheckbox = $("#checkbox-income");
+const expenseCheckbox = $("#checkbox-expense");
+
+//리스트 필터 함수
+const filterList = () => {
+  const incomeLists = $$(".history-amount.income");
+  const expenseLists = $$(".history-amount.expense");
+
+  incomeCheckbox.checked
+    ? incomeLists.forEach((list) => (list.parentNode.style.display = "flex"))
+    : incomeLists.forEach((list) => (list.parentNode.style.display = "none"));
+
+  expenseCheckbox.checked
+    ? expenseLists.forEach((list) => (list.parentNode.style.display = "flex"))
+    : expenseLists.forEach((list) => (list.parentNode.style.display = "none"));
+};
+
+//체크 박스 이벤트에 의한 필터링 헨들링 함수
+const handleFilterCheckbox = () => {
+  incomeCheckbox.addEventListener("change", filterList);
+  expenseCheckbox.addEventListener("change", filterList);
+};
+
 handleRenderInitData();
+handleFilterCheckbox();
