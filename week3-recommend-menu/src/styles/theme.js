d9b779b963d24f812bdfb8200d34c754a26d1643
Yeonseo-Jo <jys073190@gmail.com>
2023-11-10 04:45:50 +0900

    init : theme color code 추가

--- week3-recommend-menu/src/styles/theme.js
+++ week3-recommend-menu/src/styles/theme.js
@@ -1,6 +1,8 @@
 const colors = {
   black: "#000000",
   white: "#FFFFFF",
+  borderColor: "#d8d9dd",
+  hoverColor: "#646875",
   mainColor: "#ffafc2",
   pointColor: "#ff3767",
   bgColor: "#f7f7fa",
