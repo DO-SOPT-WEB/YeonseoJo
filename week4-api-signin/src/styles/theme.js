4674f3e7d8642ce752b3478b329e5590ec6af443
Yeonseo-Jo <jys073190@gmail.com>
2023-11-16 13:17:13 +0900

    init : globalStyle, theme 추가

---
+++ week4-api-signin/src/styles/theme.js
@@ -0,0 +1,14 @@
+const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  blue: "#3B77BC",
+  red: "#DE482B",
+  green: "#81C046",
+  yellow: "#FCCF03",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
